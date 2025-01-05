2e0daae8528f96cc4f5105d13bd4ed108e592408
Rohitrout416 <rohitiitrout416@gmail.com>
2025-01-05 12:53:03 +0530

    images now work

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,25 @@
+import card1 from '../img/card1.png';
+import card2 from '../img/card2.png';
+import card3 from '../img/card3.png';
+
+const images = {
+  "card1.png": card1,
+  "card2.png": card2,
+  "card3.png": card3
+};
+
 function Card(props) {
+  console.log(props.img);
   return (
     <div className="card">
       <br />
       
       <div className="text-center">
-        <img alt="card-img" src={"./img/"+props.img} className="text-center img-fluid" />
+        <img 
+          alt="card-img" 
+          src={images[props.img]} // Use mapped import
+          className="text-center img-fluid" 
+        />
       </div>
       <div className="text-center">
         <h3 className="card-title">{props.title}</h3>
@@ -17,4 +32,5 @@ function Card(props) {
     </div>
   );
 }
+
 export default Card;
