e0d285122ac6d310c5f6cef24c5bc752fe7c1117
Rohitrout416 <rohitiitrout416@gmail.com>
2025-01-05 12:02:55 +0530

    video works now

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
+import video from '../video.mp4'
+
 function Header() {
   return (
     <header>
-      <video src="./video.mp4" loop autoPlay muted></video>
+      <video src={video} loop autoPlay muted></video>
       <h1>Open Source Website</h1>
       <div className="row">
         <button className="btn" style={{ cursor: "pointer" }}>
